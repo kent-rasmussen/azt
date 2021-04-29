f7eae7ff1269ff9a6c7db136d2714a8513b17551
Kent Rasmussen <kent_rasmussen@sil.org>
2021-04-29 21:09:06 +0100

    generalize spacing

--- xmlfns.py
+++ xmlfns.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 # coding=UTF-8
 def indent(elem, level=0):
+    indentspaces=4
     """from http://effbot.org/zone/element-lib.htm#prettyprint"""
-    i = "\n" + level*"  "
+    i = "\n" + level*" "*indentspaces
     if len(elem):
         if not elem.text or not elem.text.strip():
-            elem.text = i + "  "
+            elem.text = i + " "*indentspaces
         if not elem.tail or not elem.tail.strip():
             elem.tail = i
         for elem in elem:
