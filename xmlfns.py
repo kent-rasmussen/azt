67667aeecf181f2ecaae027b65c093e3441824c6
Kent Rasmussen <kent_rasmussen@sil.org>
2021-04-29 18:21:32 +0100

    start xml functions module

---
+++ xmlfns.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# coding=UTF-8
+def indent(elem, level=0):
+    """from http://effbot.org/zone/element-lib.htm#prettyprint"""
+    i = "\n" + level*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
