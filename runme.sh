fec37ad5965e55677f0b332eb019e3355273ab06
Kent Rasmussen <kent_rasmussen@sil.org>
2021-09-14 15:11:44 +0100

    first draft of linux install script

---
+++ runme.sh
@@ -0,0 +1,8 @@
+sudo desktop-file-validate  ../../.local/share/applications/azt.desktop
+sudo update-desktop-database
+sudo apt-get install python3-pip
+python -m pip install pyaudio
+python3-tk
+python -m pip install tkinter
+
+sudo apt-get install portaudio19-dev
