f62ca2a8d54c9bc0a086c6a3929e83652f8f306d
Kent Rasmussen <kent_rasmussen@sil.org>
2021-09-21 12:16:57 +0100

    done with runme

--- runme.sh
+++
@@ -1,8 +0,0 @@
-sudo desktop-file-validate  ../../.local/share/applications/azt.desktop
-sudo update-desktop-database
-sudo apt-get install python3-pip
-python -m pip install pyaudio
-python3-tk
-python -m pip install tkinter
-
-sudo apt-get install portaudio19-dev
