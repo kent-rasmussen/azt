bf8081f69564c709efca5b80a996dcbcd6a7fc30
Kent Rasmussen <kent_rasmussen@sil.org>
2021-10-19 17:54:07 +0100

    make copy work

--- lift.py
+++ lift.py
@@ -14,6 +14,7 @@ import os
 import rx
 import logging
 import ast #For string list interpretation
+import copy
 log = logging.getLogger(__name__)
 try: #Allow this module to be used without translation
     _
