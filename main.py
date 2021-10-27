5d84b02a8a3bb3c8b4b29d2e5bcafc9ba44b57f1
Kent Rasmussen <kent_rasmussen@sil.org>
2021-10-27 11:58:38 +0100

    don't use empty data

--- main.py
+++ main.py
@@ -6324,7 +6324,7 @@ class DictbyLang(dict):
     def frame(self,framedict,langs): #langs can/should be ordered
         """the frame only applies if there is a language value; I hope that's
         what we want..."""
-        for l in [i for i in langs if i in framedict if i in self]:
+        for l in [i for i in langs if i in framedict if i in self and self[i] != []]:
             self.framed[l]=rx.framerx.sub(self[l],framedict[l])
     def __init__(self):
         super(DictbyLang, self).__init__()
