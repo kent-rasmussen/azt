2c924cb0de1b446c5e91a2a91de4357f4983b9d5
Kent Rasmussen <kent_rasmussen@sil.org>
2021-12-14 16:29:04 +0100

    would actually trigger

--- main.py
+++ main.py
@@ -6007,7 +6007,7 @@ class ExampleDict(dict):
                 log.debug("n: {}".format(n))
                 senseid=senseids[randint(0, n-1)]
             self[group]=senseid #store for next iteration
-        if senseid is None: #not self.hasglosses(senseid):
+        if tries == n*2: #senseid is None: #not self.hasglosses(senseid):
             log.info("Apparently I tried for a senseid {} times, and couldn't "
             "find one matching your needs ({}) glosses (out of {} possible "
             "senses). This is probably a systematic problem to fix.".format(
