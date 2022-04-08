5f6286d2a480e4f03ea1b2ea51f371d05984fc5b
Kent Rasmussen <kent_rasmussen@sil.org>
2022-04-08 17:02:47 +0300

    fix syntax error

--- lift.py
+++ lift.py
@@ -1885,10 +1885,10 @@ class LiftURL():
             node=[node]
         for i in node:
             log.log(4,"running kwargshaschildrenof.gen on '{}'".format(i))
-            if i is not '':
+            if i != '':
                 ii=self.children.get(i,'')
                 log.log(4,"Found '{}' this time!".format(ii))
-                if ii is not '':
+                if ii != '':
                     x+=ii
                     self.getfamilyof(ii,x)
         return x
