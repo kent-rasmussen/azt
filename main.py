c478ef324daec41338d3cd3b28f94dd97949dd82
Kent Rasmussen <kent_rasmussen@sil.org>
2022-04-08 22:25:46 +0300

    more appropriate for me...

--- main.py
+++ main.py
@@ -10846,7 +10846,8 @@ def main():
 def mainproblem():
     log.info("Starting up help line...")
     if program['testing'] and me:
-        return
+        sys.exit()
+        exit()
     file=logsetup.writelzma()
     try: #Make this work whether root has run/still runs or not.
         program['root'].winfo_exists()
