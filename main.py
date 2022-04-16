e6ed0f1b7d82ba9ec14055c0b62874c93a335fc1
Kent Rasmussen <kent_rasmussen@sil.org>
2022-04-16 14:21:35 +0300

    better log

--- main.py
+++ main.py
@@ -8568,7 +8568,8 @@ class ExampleDict(dict):
             return
         framed=self.datadict.getframeddata(senseid=senseid,check=check)
         if framed is None:
-            log.info("getframeddata didn't result in a frame!")
+            log.info("getframeddata didn't result in a frame! ({};{})".format(
+                                                                senseid,check))
             return
         # log.info("exampletypeok framed: {}".format(framed))
         if kwargs['wglosses'] and not self.hasglosses(framed):
