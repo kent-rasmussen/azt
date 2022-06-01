772fbbda6f7fe78a42fc9e1ea39d814d208bdead
Kent Rasmussen <kent_rasmussen@sil.org>
2022-06-01 12:38:01 +0100

    get the right level (below ps)

--- main.py
+++ main.py
@@ -10281,8 +10281,9 @@ class ToneFrames(dict):
         super(ToneFrames, self).__init__()
         for k in dict:
             self[k]=dict[k]
-            if 'field' not in self[k]:
-                self[k]['field']='lc'
+            for name in self[k]:
+                if 'field' not in self[k][name]:
+                    self[k][name]['field']='lc'
 class StatusDict(dict):
     """This stores and returns current ps and profile only; there is no check
     here that the consequences of the change are done (done in check)."""
