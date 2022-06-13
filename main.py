9dd16719370a06d2ff666eb1bea01adba3a2e0d3
Kent Rasmussen <kent_rasmussen@sil.org>
2022-06-13 09:28:10 +0100

    reduce button size

--- main.py
+++ main.py
@@ -9986,7 +9986,7 @@ class ToneGroupButtonFrame(ui.Frame):
                         **tinyfontkwargs)
         bct=ui.ToolTip(bc,_("Change example word"))
     def unsortbutton(self):
-        t=_("<= remove *this* *word* from \nthe group (sort into another, later)")
+        t=_("<= resort *this* *word*")
         usbkwargs=self.buttonkwargs()
         usbkwargs['wraplength']=usbkwargs['wraplength']*2/3
         b_unsort=ui.Button(self,text = t,
