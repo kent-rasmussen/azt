aa8f70bc2020110e7cdf5bb9010dc39abd6bb3a5
Kent Rasmussen <kent_rasmussen@sil.org>
2022-08-27 12:14:38 +0100

    use real fns

--- main.py
+++ main.py
@@ -7312,7 +7312,7 @@ class Report(object):
         check=kwargs.get('check',self.params.check())
         group=kwargs.get('group',self.status.group())
         ftype=kwargs.get('ftype',self.params.ftype())
-        if isinteger(group) or (hasattribute(self,'groupcomparison') and
+        if isinteger(group) or (hasattr(self,'groupcomparison') and
                                 isinteger(self.groupcomparison)):
             t=_("Skipping check")+' '+t+' '+_("because it would break the regex")
         xlp.Paragraph(parent,t)
