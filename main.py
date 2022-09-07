88190be8f51968610f5278f5c3711f9bf59a0c7c
Kent Rasmussen <kent_rasmussen@sil.org>
2022-09-07 20:03:42 +0100

    Autocommit from AZT

--- main.py
+++ main.py
@@ -12040,7 +12040,7 @@ class Repository(object):
             self.exewarning()
             return #before getting a file list!
         self.usernameargs=self.getusernameargs()
-        self._remotes={} #don't die right away
+        # self._remotes={} #don't die right away
         self.getfiles()
         self.ignorecheck()
         log.info("{} repository object initialized, with {} files."
