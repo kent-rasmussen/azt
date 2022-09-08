2bbcccff029a4edd674bfc8825fb6085855d84ac
Kent Rasmussen <kent_rasmussen@sil.org>
2022-09-08 19:30:58 +0100

    works for empty fields

--- lift.py
+++ lift.py
@@ -972,9 +972,9 @@ class Lift(object): #fns called outside of this class call self.nodes here.
                                 'lexical-unit']}
         fieldopts.update(
                             {f:'field[@type="{}"]'.format(f)
-                                    for f in self.fieldnames[l]
                                     for l in self.analangs
                                     if l in self.fieldnames
+                                    for f in self.fieldnames[l]
                                     })
         # log.info("fieldopts: {}".format(fieldopts))
         for lang in self.analangs:
