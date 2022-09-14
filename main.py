aa8463e895d7a6ec5e1eb5a54f6435ea3bfa6749
Kent Rasmussen <kent_rasmussen@sil.org>
2022-09-14 07:23:58 +0100

    backgrounding seems to be working

--- main.py
+++ main.py
@@ -7124,6 +7124,7 @@ class Report(object):
             #             {'ps':ps,'profile':p,'usegui':False} for p in d[ps]])
     def tonegroupreportmulti(self,**kwargs):
         # threading.Thread(target=self.tonegroupreport,kwargs=kwargs).start()
+        start_time=nowruntime()
         kwargs['usegui']=False
         if hasattr(self.settings,'maxpss'):
             pss=self.slices.pss()[:self.settings.maxpss]
@@ -7137,6 +7138,7 @@ class Report(object):
                 d[ps]=[self.slices.profile()]
         log.info("Starting background reports for {}".format(d))
         unbackground=[]
+        all=[]
         for ps in pss:
             for profile in d[ps]:
                 kwargs['ps']=ps
@@ -7144,18 +7146,28 @@ class Report(object):
                 t=multiprocessing.Process(target=self.tonegroupreport,
                                             kwargs=kwargs)
                 t.start()
+                log.info(_("Starting XLP background report with kwargs {}"
+                            ).format(kwargs))
+                all+=[kwargs.copy()]
                 time.sleep(0.2) #give it 200ms before checking if it returned already
                 if not t.is_alive():
                     ErrorNotice(_("Looks like that didn't work; you may need "
                                     "to run a report first, or not do it in "
                                     "the background ({})."
                                 ).format(kwargs))
-                    unbackground+=[kwargs]
+                    unbackground+=[kwargs.copy()]
+        done=all[:]
+        for k in unbackground:
+            done.remove(k)
+        logfinished(start_time,msg="setting up background reports {}".format(done))
+        log.info(_("Starting reports that didn't work in the background: {}").format(unbackground))
         for kwargs in unbackground:
             self.tonegroupreport(**kwargs) #run what failed in background here
+        logfinished(start_time,msg="all reports ({})".format(all))
     def tonegroupreport(self,usegui=True,**kwargs):
         """This should iterate over at least some profiles; top 2-3?
         those with 2-4 verified frames? Selectable with radio buttons?"""
+        start_time=nowruntime()
         #default=True redoes the UF analysis (removing any joining/renaming)
         ftype=kwargs.get('ftype',self.params.ftype())
         def examplestoXLP(examples,parent,senseid):
@@ -7420,6 +7432,7 @@ class Report(object):
         ps2=xlp.Paragraph(s2,text=ptext)
         xlpr.close(me=me)
         text=_("Finished in {} seconds.").format(nowruntime()-start_time)
+        text=logfinished(start_time,msg="report {} {}".format(ps,profile))
         logfinished(start_time)
         output(window,r,text)
         text=_("(Report is also available at ({})").format(self.tonereportfile)
