fa6279f7c5d55bcf9ab6e4d132257828f69af190
Kent Rasmussen <kent_rasmussen@sil.org>
2022-09-15 08:05:10 +0100

    other people shouldn't be merging

--- main.py
+++ main.py
@@ -12184,7 +12184,7 @@ class Repository(object):
                     ErrorNotice(txt)
                 except (RuntimeError,AssertionError):
                     log.info(txt)
-            if iwascalledby in ["pull"] and self.code == 'git':
+            if me and iwascalledby in ["pull"] and self.code == 'git':
                 log.info(_("Call to {} ({}) gave error: \n{}\nMerging.").format(
                                                         self.repotypename,
                                                         ' '.join(args),
