244c499d1b301137425f107b7a5f05e879d7325a
Kent Rasmussen <kent_rasmussen@sil.org>
2022-12-15 11:19:38 +0100

    break up script for readability

--- installfiles/RunMetoInstall.bat
+++ installfiles/RunMetoInstall.bat
@@ -15,10 +15,14 @@ ECHO not moving to "%userprofile%/Downloads"
 ECHO moving to %~dp0 (where you downloaded the script)
 cd /d %~dp0
 
+If exist python-3.6.8-amd64.exe (
+ECHO python-3.6.8-amd64.exe is there!
+) ELSE (
 ECHO Downloading Python 3.6.8 (31830944; 30M)...
 ECHO Check that your internet is on and
 pause
-If exist python-3.6.8-amd64.exe (ECHO python-3.6.8-amd64.exe is there!) ELSE (powershell.exe -noprofile -command "Invoke-WebRequest 'https://www.python.org/ftp/python/3.6.8/python-3.6.8-amd64.exe' -OutFile 'python-3.6.8-amd64.exe'")
+powershell.exe -noprofile -command "Invoke-WebRequest 'https://www.python.org/ftp/python/3.6.8/python-3.6.8-amd64.exe' -OutFile 'python-3.6.8-amd64.exe'"
+)
 
 ECHO Installing Python 3.6.8
 ECHO
@@ -37,10 +41,14 @@ ECHO Otherwise, you should be fine with all default options
 ECHO
 start python-3.6.8-amd64.exe
 
+If exist Git-2.33.0.2-64-bit.exe (
+ECHO Git-2.33.0.2-64-bit.exe is there!
+) ELSE (
 ECHO Downloading Git 2.33.0.2 (50101024; 48M)...
 ECHO Check that your internet is on and
 pause
-If exist Git-2.33.0.2-64-bit.exe (ECHO Git-2.33.0.2-64-bit.exe is there!) ELSE (powershell.exe -noprofile -command "Invoke-WebRequest 'https://github.com/git-for-windows/git/releases/download/v2.33.0.windows.2/Git-2.33.0.2-64-bit.exe' -OutFile 'Git-2.33.0.2-64-bit.exe'")
+powershell.exe -noprofile -command "Invoke-WebRequest 'https://github.com/git-for-windows/git/releases/download/v2.33.0.windows.2/Git-2.33.0.2-64-bit.exe' -OutFile 'Git-2.33.0.2-64-bit.exe'"
+)
 
 ECHO waiting for you to finish installing Python 3.6.8
 pause
@@ -85,19 +93,32 @@ pause
 
 cd /d "%userprofile%/Downloads"
 
+If exist XLingPaper3-10-1XXEPersonalEditionFullSetup.exe (
+ECHO XLingPaper3-10-1XXEPersonalEditionFullSetup.exe is there!
+) ELSE (
 ECHO Downloading XLingPaper 3.10...
-If exist XLingPaper3-10-1XXEPersonalEditionFullSetup.exe (ECHO XLingPaper3-10-1XXEPersonalEditionFullSetup.exe is there!) ELSE (powershell.exe -noprofile -command "Invoke-WebRequest 'https://software.sil.org/downloads/r/xlingpaper/XLingPaper3-10-1XXEPersonalEditionFullSetup.exe' -OutFile 'XLingPaper3-10-1XXEPersonalEditionFullSetup.exe'")
+powershell.exe -noprofile -command "Invoke-WebRequest 'https://software.sil.org/downloads/r/xlingpaper/XLingPaper3-10-1XXEPersonalEditionFullSetup.exe' -OutFile 'XLingPaper3-10-1XXEPersonalEditionFullSetup.exe'"
+)
 
 ECHO Installing XLingPaper 3.10
 start XLingPaper3-10-1XXEPersonalEditionFullSetup.exe
 
+If exist praat6218_win64.zip (
+ECHO praat6218_win64.zip is there!
+) ELSE (
 ECHO Downloading Praat 6218...
-If exist praat6218_win64.zip (ECHO praat6218_win64.zip is there!) ELSE (powershell.exe -noprofile -command "Invoke-WebRequest 'https://www.fon.hum.uva.nl/praat/praat6218_win64.zip' -OutFile 'praat6218_win64.zip'")
+powershell.exe -noprofile -command "Invoke-WebRequest 'https://www.fon.hum.uva.nl/praat/praat6218_win64.zip' -OutFile 'praat6218_win64.zip'"
+)
+ECHO installing Praat to %ProgramFiles%
 tar -xvf praat6218_win64.zip -C "%ProgramFiles%"
 setx path "%path%;%ProgramFiles%"
 
+If exist Mercurial-6.0-x64.exe (
+ECHO Mercurial-6.0-x64.exe is there!
+) ELSE (
 ECHO Downloading Mercurial 6.0...
-If exist Mercurial-6.0-x64.exe (ECHO Mercurial-6.0-x64.exe is there!) ELSE (powershell.exe -noprofile -command "Invoke-WebRequest 'https://www.mercurial-scm.org/release/windows/Mercurial-6.0-x64.exe' -OutFile 'Mercurial-6.0-x64.exe'")
+powershell.exe -noprofile -command "Invoke-WebRequest 'https://www.mercurial-scm.org/release/windows/Mercurial-6.0-x64.exe' -OutFile 'Mercurial-6.0-x64.exe'"
+)
 
 ECHO Installing Mercurial 6.0
 start Mercurial-6.0-x64.exe
@@ -109,7 +130,11 @@ goto end
 ECHO This doesn't seem to be running as administrator.
 ECHO Trying to start an admin command prompt. If that doesn't work, please
 ECHO run this bat file as administrator yourself.
-if runas /env /user:domain\Administrator %~0 (EHCO Worked!) ELSE (runas /env /user:Administrator %~0)
+if runas /env /user:domain\Administrator %~0 (
+ECHO Worked!
+) ELSE (
+runas /env /user:Administrator %~0
+)
 echo Stopping here just in case you need to read anything above; we're done!
 Pause
 :end
