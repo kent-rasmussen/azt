4965edf7f9cc823d40c0a116da8afa937a92971a
Kent Rasmussen <kent_rasmussen@sil.org>
2022-12-16 15:15:57 +0100

    log less

--- main.py
+++ main.py
@@ -13864,8 +13864,8 @@ def indenteddict(indict):
             # config[s][j]='\n'.join(['{'+i+':'+str(v[j][i])+'}'
             #                             for i in v[j].keys()])
             if True in [isinstance(i, dict) for i in indict[j].values()]:
-                log.info("printing double indented dict for {}: {} "
-                            "keys".format(j,indict[j].keys()))
+                # log.info("printing double indented dict for {}: {} "
+                #             "keys".format(j,indict[j].keys()))
                 outdict[j]='{'+',\n'.join(
                     [quote(k)+":{"+',\n\t'.join(
                                         [quote(i)+":"+quote(indict[j][k][i])
