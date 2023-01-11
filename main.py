b7779429e98b04b1c0e44713e51e392533495bd5
Kent Rasmussen <kent_rasmussen@sil.org>
2023-01-11 08:45:48 +0100

    move to utilities

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ import parser
 # import profiles
 import setdefaults
 import xlp
+from utilities import *
 try:
     import sound
     import transcriber
@@ -14571,14 +14572,6 @@ def handle_exception(exc_type, exc_value, exc_traceback):
     log.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
 def openweburl(url):
     webbrowser.open_new(url)
-def ofromstr(x):
-    """This interprets a string as a python object, if possible"""
-    """This is needed to interpret [x,y] as a list and {x:y} as a dictionary."""
-    try:
-        return ast.literal_eval(x)
-    except (SyntaxError,ValueError) as e:
-        # log.debug("Assuming ‘{}’ is a string ({})".format(x,e))
-        return x
 def tryrun(cmd):
     try:
         cmd()
