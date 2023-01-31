6a82ae66077b413a65d308ea5639540095f20738
Kent Rasmussen <kent_rasmussen@sil.org>
2023-01-31 09:58:47 +0100

    works for now

--- xmletfns.py
+++ xmletfns.py
@@ -1,5 +1,15 @@
+#!/usr/bin/env python3
+# coding=UTF-8
 from xml.etree import ElementTree as ET
+import logsetup
+log=logsetup.getlog(__name__)
+import file
+import urllib
 
+def readxml(filename):
+    tree=ET.parse(filename)
+    nodes=tree.getroot()
+    return tree,nodes
 def iselement(n):
     return isinstance(n,ET.Element)
 def prettyprint(node):
@@ -23,3 +33,36 @@ def prettyprint(node):
             t=t-1
     do(node,t)
     return '\n'.join(lines)
+def getxmlns(nodes):
+    xmlns=set()
+    results=nodes.findall(".")
+    for r in results:
+        n=r.get("xmlns:xi")
+        if n:
+            xmlns+=n
+    print(xmlns)
+def iterateforincludes(node,ns,results=[]):
+    results+=node.findall("xi:include",ns)
+    # log.info("{} ({}): {}".format(node,len(results),results))
+    for child in node:
+        if child: #i.e., has children
+            iterateforincludes(child,ns,results)
+    return results
+def getincluded(filename,iterated=False):
+    # Each new files starts here
+    filename=urllib.parse.unquote(str(filename), encoding='utf-8', errors='replace')
+    # log.info("Looking at filename {}".format(filename))
+    t,n=readxml(filename)
+    # ns=getxmlns(n)
+    dir=str(file.getfilenamedir(filename))
+    # log.info("In filename dir {}".format(dir))
+    ns={'xi':"http://www.w3.org/2001/XInclude"}
+    results=iterateforincludes(n,ns,[])
+    # log.info("{}: {}".format(len(results),results))
+    for r in results:
+        r=file.getdiredrelURL(dir,r.get('href'))
+        log.info("Found reference to filename {}".format(r))
+        getincluded(r)
+if __name__ == '__main__':
+    f='/home/kentr/Assignment/Production/CACBLD/Emic First Phonology_paper.xml'
+    getincluded(f)
