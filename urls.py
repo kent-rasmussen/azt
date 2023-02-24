e7a0b0807d4a542e15e690a42fe5382fa175ccf0
Kent Rasmussen <kent_rasmussen@sil.org>
2023-02-24 16:12:43 +0100

    url chnges

--- urls.py
+++ urls.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 # coding=UTF-8
-import urllib.request
-import urllib.parse
-urlencode=urllib.parse.urlencode
-urlopen=urllib.request.urlopen
-urllib.request.Request
+import urllib.request as request
+import urllib.parse as parse
+urlencode=parse.urlencode
+urlopen=request.urlopen
+import urllib3
+http = urllib3.PoolManager()
+# resp = http.request
+# urllib.request.Request
