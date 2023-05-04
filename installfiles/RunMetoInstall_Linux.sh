5ce4260f61d2caa96e08de4b56629dcc68767517
Kent Rasmussen <kent_rasmussen@sil.org>
2023-05-04 09:42:31 +0100

    reorganize to get update in time, after key

--- installfiles/RunMetoInstall_Linux.sh
+++ installfiles/RunMetoInstall_Linux.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
-sudo apt-get install git python3-tk python3-pip portaudio19-dev texlive-xetex
-python3 -m pip install pyaudio lxml Pillow
-cd;git clone https://github.com/kent-rasmussen/azt.git;cd -
-echo "The following assumes you have a debian system; if you don't, install"
-echo "the SIL package repository manually (instructions at https://packages.sil.org/)"
 (wget -O- https://packages.sil.org/keys/pso-keyring-2016.gpg | sudo tee /etc/apt/trusted.gpg.d/pso-keyring-2016.gpg)&>/dev/null
 (. /etc/os-release && sudo tee /etc/apt/sources.list.d/packages-sil-org.list>/dev/null <<< "deb http://packages.sil.org/$ID $VERSION_CODENAME main")
 sudo apt update
+sudo apt-get install git python3-tk python3-pip portaudio19-dev texlive-xetex && python3 -m pip install pyaudio lxml Pillow
+cd;git clone https://github.com/kent-rasmussen/azt.git;cd -
+echo "The following assumes you have a debian system; if you don't, install"
+echo "the SIL package repository manually (instructions at https://packages.sil.org/)"
 sudo apt-get install fonts-sil-charis
 # wget https://github.com/kent-rasmussen/azt/blob/main/installfiles/azt.desktop?raw=true -O azt.desktop
 cp ${HOME}/azt/installfiles/azt.desktop $HOME/.local/share/applications/
