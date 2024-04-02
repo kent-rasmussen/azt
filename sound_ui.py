44440cfdb02e3c2614bdc82b452b1cefe56b0397
Kent Rasmussen <kent_rasmussen@sil.org>
2024-04-02 10:04:59 -0500

    initial drafts for new initiative

---
+++ sound_ui.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+# coding=UTF-8
+import logsetup
+log=logsetup.getlog(__name__)
+logsetup.setlevel('INFO',log) #for this file
+import ui_tkinter as ui
+import sound
+import file
+class RecordButtonFrame(ui.Frame):
+    def _start(self, event):
+        log.log(3,"Asking PA to record now")
+        self.recorder=sound.SoundFileRecorder(self._filenameURL,self.pa,
+                                            self.soundsettings)
+        log.debug("PA recorder made OK")
+        self.recorder.start()
+    def _stop(self, event):
+        try:
+            self.recorder.stop()
+        except:
+            log.info("Couldn't stop recorder; was it on?")
+        """This is done in advance of recording now:"""
+        self.b.destroy()
+        self.makeplaybutton()
+        self.makedeletebutton()
+        self.addlink()
+    def _redo(self, event):
+        log.log(3,"I'm deleting the recording now")
+        self.p.destroy()
+        self.makerecordbutton()
+        self.r.destroy()
+    def makebuttons(self):
+        if file.exists(self._filenameURL):
+            self.makeplaybutton()
+            self.makedeletebutton()
+            self.addlink()
+        else:
+            self.makerecordbutton()
+    def makerecordbutton(self):
+        self.b=ui.Button(self, command=self.function,
+                        image='record',
+                        ipadx=20, ipady=15
+                        )
+        self.b.grid(row=0, column=0,sticky='w')
+        self.b.bind('<ButtonPress-1>', self._start)
+        self.b.bind('<ButtonRelease-1>', self._stop)
+        self.bt=ui.ToolTip(self.b,_("press-speak-release"))
+    def _play(self,event=None):
+        log.debug("Asking PA to play now")
+        self.player=sound.SoundFilePlayer(self._filenameURL,self.pa,
+                                            self.soundsettings)
+        tryrun(self.player.play)
+    def makeplaybutton(self):
+        self.p=ui.Button(self, text='‣', command=self._play,
+                        font='readbig',
+                        ipadx=20, ipady=0
+                        )
+        #Not using these for now
+        # self.p.bind('<ButtonPress>', self._play)
+        # self.p.bind('<ButtonRelease>', self.function)
+        self.p.grid(row=0, column=1,sticky='w')
+        pttext=_("Click to hear")
+        if program['praat']:
+            pttext+='; '+_("right click to open in praat")
+            self.p.bind('<Button-3>',lambda x: praatopen(self._filenameURL))
+        self.pt=ui.ToolTip(self.p,pttext)
+    def makedeletebutton(self):
+        self.r=ui.Button(self,text='×',font='read',command=self.function,
+                        row=0, column=2, sticky='nsw')
+        self.r.bind('<ButtonRelease-1>', self._redo)
+        self.rt=ui.ToolTip(self.r,_("Try again"))
+        self.r.update_idletasks()
+    def function(self):
+        pass
+    def addlink(self):
+        if self.test:
+            return
+        program['db'].addmediafields(self.node,self.filename,
+                                program['params'].audiolang,
+                                # ftype=ftype,
+                                write=False)
+        self.task.maybewrite()
+        program['status'].last('recording',update=True)
+    def __init__(self,parent,task,node=None,**kwargs): #filenames
+        """Uses node to make framed data, just for soundfile name"""
+        """Without node, this just populates a sound file, with URL as
+        provided. The LIFT link to that sound file should already be there."""
+        # This class needs to be cleanup after closing, with check.donewpyaudio()
+        """Originally from https://realpython.com/playing-and-recording-
+        sound-python/"""
+        self.id=id
+        self.task=task
+        try:
+            task.pyaudio.get_format_from_width(1) #get_device_count()
+        except:
+            task.pyaudio=sound.AudioInterface()
+        self.pa=task.pyaudio
+        if not hasattr(task,'soundsettings'):
+            if not hasattr(program['settings'],'soundsettings'):
+                task.loadsoundsettings()
+            self.soundsettings=program['settings'].soundsettings
+        else:
+            self.soundsettings=task.soundsettings
+        self.callbackrecording=True
+        self.chunk = 1024  # Record in chunks of 1024 samples (for block only)
+        self.channels = 1 #Always record in mono
+        self.test=kwargs.pop('test',None)
+        if node:# and framed.framed != 'NA':
+            task.makeaudiofilename(node) #should fill out the following
+            self.filename=node.textvaluebylang(program['params'].audiolang)
+            if not self.filename: #should have above or below
+                self.filename=node.audiofilenametoput
+            self._filenameURL=node.audiofileURL
+            self.node=node
+        elif self.test:
+            self.filename=self._filenameURL='test_{}_{}.wav'.format(
+                                self.soundsettings.fs,
+                                self.soundsettings.sample_format)
+        else:
+            t=_("No framed value, nor testing; can't continue...")
+            log.error(t)
+            ui.Label(self,text=t,borderwidth=1,font='default',
+                    relief='raised').grid(row=0,column=0)
+        ui.Frame.__init__(self,parent, **kwargs)
+        """These need to happen after the frame is created, as they
+        might cause the init to stop."""
+        if not self.test and not program['settings'].audiolang:
+            tlang=_("Set audio language to get record buttons!")
+            log.error(tlang)
+            ui.Label(self,text=tlang,borderwidth=1,
+                relief='raised' #flat, raised, sunken, groove, and ridge
+                ).grid(row=0,column=0)
+            return
+        if None in [self.soundsettings.fs,
+                    self.soundsettings.sample_format,
+                    self.soundsettings.audio_card_in,
+                    self.soundsettings.audio_card_out]:
+            text=_("Set all sound card settings"
+                    "\n(Do|Recording|Sound Card Settings)"
+                    "\nand a record button will be here.")
+            log.debug(text)
+            ui.Label(self,text=text,borderwidth=1,
+                relief='raised' #flat, raised, sunken, groove, and ridge
+                ).grid(row=0,column=0)
+            return
+        self.makebuttons()
+class Task():
+    def __init__(self):
+        self.pyaudio=sound.AudioInterface()
+        self.soundsettings=sound.SoundSettings(self.pyaudio)
+        self.audiolang=True
+if __name__ == "__main__":
+    try:
+        _
+    except:
+        def _(x):
+            return x
+    r=ui.Root()
+    r.title('Sound UI')
+    # program['settings']
+    task=Task()
+    ui.Label(r,text="test Frame",row=0,column=0)
+    RecordButtonFrame(r,task,test=True,row=1,column=0)
+    r.deiconify()# soundsettings=sound.SoundSettings()
+    r.mainloop()
---
+++ transcriberCV.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python3
+# coding=UTF-8
+try:
+    import sound
+except ModuleNotFoundError:
+    raise
+import ui_tkinter as ui
+import rx
+import logsetup
+log=logsetup.getlog(__name__)
+logsetup.setlevel('INFO',log) #for this file
+
+class Transcriber(ui.Frame):
+    def addchar(self,x):
+        if x in ['','∅'] or self.formfield.get() == '∅':
+            self.formfield.delete(0,ui.END)
+        if x:
+            self.formfield.insert(ui.INSERT,x) #could also do tkinter.END
+        if x == '': #if cleared, be ready to type
+            self.formfield.focus_set()
+        self.updatelabels()
+    def updatelabels(self,event=None):
+        a=self.newname.get()
+        if set(['˥','˦','˧','˨','˩']) & set(a):
+            x=self.hash_t.sub('T',self.newname.get())
+            y=self.hash_sp.sub('#',x)
+            z=self.hash_nbsp.sub('.',y)
+            self.namehash.set(z)
+        else:
+            self.namehash.set('')
+        self.labelcompiled=False
+    def playbeeps(self,pitches):
+        if not self.labelcompiled:
+            self.beeps.compile(pitches)
+        self.beeps.play()
+    def configurebeeps(self,event=None):
+        def higher():
+            self.beeps.higher()
+            self.labelcompiled=False
+        def lower():
+            self.beeps.lower()
+            self.labelcompiled=False
+        def wider():
+            self.beeps.wider()
+            self.labelcompiled=False
+        def narrower():
+            self.beeps.narrower()
+            self.labelcompiled=False
+        def shorter():
+            self.beeps.shorter()
+            self.labelcompiled=False
+        def longer():
+            self.beeps.longer()
+            self.labelcompiled=False
+        p=self.parent
+        while not (isinstance(p,ui.Window) or isinstance(p,ui.Root)): # windows need window parents
+            p=p.parent
+        w=ui.Window(p, title=_("Configure Tone Beeps"))
+        w.attributes("-topmost", True)
+        ui.Button(w.frame,text=_("pitch up"),cmd=higher,
+                        row=0,column=0)
+        ui.Button(w.frame,text=_("pitch down"),cmd=lower,
+                        row=1,column=0)
+        ui.Button(w.frame,text=_("more H-L difference"),cmd=wider,
+                        row=0,column=1)
+        ui.Button(w.frame,text=_("less H-L difference"),cmd=narrower,
+                        row=1,column=1)
+        ui.Button(w.frame,text=_("slower"),cmd=longer,
+                        row=2,column=0)
+        ui.Button(w.frame,text=_("faster"),cmd=shorter,
+                        row=2,column=1)
+    def __init__(self, parent, initval=None, soundsettings=None, **kwargs):
+        self.newname=ui.StringVar(value=initval)
+        self.namehash=ui.StringVar()
+        # self.hash_t,self.hash_sp,self.hash_nbsp=rx.tonerxs()
+        self.pyaudio=sound.AudioInterface()
+        if soundsettings:
+            self.soundsettings=soundsettings
+        else:
+            self.soundsettings=sound.SoundSettings(self.pyaudio)
+        # self.beeps=sound.BeepGenerator(pyAudio=self.pyaudio,
+        #                                     settings=self.soundsettings)
+        if 'chars' in kwargs and kwargs['chars'] and type(kwargs['chars']) is list:
+            chars=kwargs.pop('chars')
+            if len(chars)> 50:
+                root=False #don't make it square, but 4:3
+            elif len(chars) >25:
+                root=int(len(chars)**(1/2))+2 #make it square
+            else:
+                root=7 #at least this many columns
+            if root and len(chars)%root:
+                # log.info("{} indivisible by {}!".format(len(chars),root))
+                nrows=len(chars)//root+1
+                ncols=len(chars)//nrows+1
+            elif root:
+                # log.info("{} divisible by {}!".format(len(chars),root))
+                ncols=root
+                nrows=len(chars)//root
+            else: #this isn't the right math, but close enough for now
+                x=9
+                y=3
+                ncols=int((len(chars)*x/y)**(1/2))+1
+                nrows=len(chars)//ncols
+                log.info("chars: {}; ncols: {}; nrows: {}; x:y={}"
+                        "".format(len(chars),ncols,nrows,ncols/nrows))
+            chars+=['∅']
+        else:
+            chars=kwargs.pop('chars',None) #in case it is None/0/False, etc.
+            tonechars=['[', '˥', '˦', '˧', '˨', '˩', ']']
+            spaces=[' ',' ']
+            chars=tonechars+spaces
+            ncols=7
+            nrows=1
+        clear=['']
+        ui.Frame.__init__(self, parent, **kwargs)
+        buttonframe=ui.Frame(self, row=0, column=0, sticky='new')
+        log.info("Transcriber using {} rows, {} columns".format(nrows,ncols))
+        for char in chars+clear:
+            if char == ' ':
+                text=_('syllable break')
+                column=0
+                columnspan=int(ncols/2)+1
+                row=1
+            elif char == ' ':
+                text=_('word break')
+                columnspan=int(ncols/2)
+                column=columnspan+1
+                row=1
+            elif char == '∅':
+                text=_('no segments')
+                columnspan=ncols
+                column=0
+                row=nrows+2
+            elif char == '':
+                text=_('clear entry')
+                column=0
+                columnspan=ncols
+                row=nrows+1
+            else:
+                column=chars.index(char)%ncols
+                text=char
+                columnspan=1
+                row=chars.index(char)//ncols
+            # if char in chars:
+            #     log.info("Making Button {} at row={}, column={}".format(chars.index(char),row, column))
+            ui.Button(buttonframe,text = text,
+                        command = lambda x=char:self.addchar(x),
+                        anchor ='c',
+                        row=row,
+                        column=column,
+                        sticky='nsew',
+                        norender=True,
+                        columnspan=columnspan
+                        )
+        fieldframe=ui.Frame(self,
+                            row=1,column=0,sticky='new'
+                            )
+        self.formfield = ui.EntryField(fieldframe,textvariable=self.newname,
+                                    row=0,column=0,sticky='new',
+                                    font='readbig')
+        self.formfield.bind('<KeyRelease>', self.updatelabels) #apply function after key
+        self.formfieldplay= ui.Button(fieldframe,text=_('‣'),
+                            cmd=lambda:self.playbeeps(self.newname.get()),
+                            font='tiny',
+                            sticky='ns',
+                            row=0, column=1)
+        self.formfieldplay.bind('<Button-3>', self.configurebeeps)
+        tt=_("Left click to play, \nRight click to configure")
+        ui.ToolTip(self.formfieldplay, text=tt)
+        self.formhashlabel=ui.Label(fieldframe,
+                                textvariable=self.namehash,
+                                anchor ='c',
+                                row=1,column=0,sticky='new'
+                                )
+        # fieldframe.grid_columnconfigure(0, weight=1)
+        self.updatelabels()
+if __name__ == "__main__":
+    try:
+        _
+    except:
+        def _(x):
+            return x
+    r=ui.Root()
+    r.title('Transcriber')
+    Transcriber(r,initval='˥˥ ˩˩ ˧˧',column=1,row=1)
+    r.deiconify()# soundsettings=sound.SoundSettings()
+    r.mainloop()
