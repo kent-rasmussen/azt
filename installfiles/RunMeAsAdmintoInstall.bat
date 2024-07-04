04673063e0c75a17850c51bb54b4ec0e4e4697fe
Kent Rasmussen <kent_rasmussen@sil.org>
2024-07-04 16:40:31 -0500

    try tabs again

--- installfiles/RunMeAsAdmintoInstall.bat
+++ installfiles/RunMeAsAdmintoInstall.bat
@@ -16,18 +16,18 @@ set xlpurl=https://software.sil.org/downloads/r/xlingpaper/%xlpfilename%
 set hgversion=6.0
 set hgfilename=Mercurial-%hgversion%-x64.exe
 set hgurl=https://www.mercurial-scm.org/release/windows/%hgfilename%
-
+set tab=<tab>
 ECHO:
 ECHO:
 ECHO:
 ECHO:
 ECHO A-Z+T Install batch file
 ECHO This script installs stuff --it must be run **As Administrator**! It will:
-ECHO \tWill download and install Python %pythonversion%
-ECHO \tWill download and install Git %gitversion%
-ECHO \tCreate another file (also **run as administrator**) that will
-ECHO \t\tWill clone/download A-Z+T source to azt directory on your desktop
-ECHO \t\tWill create a shortcut to run AZT on your desktop
+ECHO,%tab%Will download and install Python %pythonversion%
+ECHO,%tab%Will download and install Git %gitversion%
+ECHO,%tab%Create another file (also **run as administrator**) that will
+ECHO,%tab%%tab%Will clone/download A-Z+T source to azt directory on your desktop
+ECHO,%tab%%tab%Will create a shortcut to run AZT on your desktop
 ECHO Error Level is %errorlevel%
 whoami /groups | find "S-1-16-12288"
 if errorlevel 1 goto NotAdmin
