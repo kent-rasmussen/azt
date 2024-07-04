aeb62a82448ade0c3913b14ef7284c4b04fd3cb0
Kent Rasmussen <kent_rasmussen@sil.org>
2024-07-04 15:29:39 -0500

    debugging; mostly nit picky and cleanup

--- installfiles/RunMeAsAdmintoInstall.bat
+++ installfiles/RunMeAsAdmintoInstall.bat
@@ -23,13 +23,13 @@ whoami /groups | find "S-1-16-12288"
 if errorlevel 1 goto NotAdmin
 ECHO Looks like I'm running As Administrator.
 ECHO Using user profile %userprofile%
-ECHO moving to %~dp0 (where you downloaded the script)
+ECHO moving to %~dp0 ^(where you downloaded the script^)
 cd /d %~dp0
 
 If exist %pythonfilename% (
 ECHO %pythonfilename% is there!
 ) ELSE (
-ECHO Downloading Python %pythonversion% %pythonsize%...
+ECHO Downloading Python %pythonversion% %pythonsize:"=%...
 ECHO Check that your internet is on and
 pause
 powershell.exe -noprofile -command "Invoke-WebRequest %pythonurl% -OutFile %pythonfilename%"
@@ -58,18 +58,19 @@ ECHO:
 REM start %pythonfilename% /?
 
 ::do one of these (/wait allows user to see whatever before going on):
-::this shows the user what?
-start /wait %pythonfilename% SimpleInstall=1 SimpleInstallDescription="Installing Python %pythonversion%" PrependPath=1 Include_pip=1 InstallAllUsers=1 Include_launcher=1 InstallLauncherAllUsers=1 Include_test=0
+::this shows the user "Install: use settings preselected by your administrator"
+::start /wait %pythonfilename% SimpleInstall=1 SimpleInstallDescription="Installing Python %pythonversion%" PrependPath=1 Include_pip=1 InstallAllUsers=1 Include_launcher=1 InstallLauncherAllUsers=1 Include_test=0
 
 ::this shows the user nothing
 ::start /wait %pythonfilename% /quiet PrependPath=1 Include_pip=1 InstallAllUsers=1 Include_launcher=1 InstallLauncherAllUsers=1 Include_test=0
 
 ::this shows the user progress with a cancel button
-::start /wait %pythonfilename% /passive PrependPath=1 Include_pip=1 InstallAllUsers=1 Include_launcher=1 InstallLauncherAllUsers=1 Include_test=0
+start /wait %pythonfilename% /passive PrependPath=1 Include_pip=1 InstallAllUsers=1 Include_launcher=1 InstallLauncherAllUsers=1 Include_test=0
 
 echo python installed; path test next
 set var=LongPathsEnabled
 set longpathsOK=
+set regkey=HKLM
 ::This list of registries should be in priority order; the first found is used.
 for %%k in (HKLM,HKCU,HKCR,HKU,HKCC) do (
   reg query %%k\SYSTEM\CurrentControlSet\Control\FileSystem /v %var%
@@ -98,10 +99,11 @@ for %%k in (HKLM,HKCU,HKCR,HKU,HKCC) do (
   :finalokcheck
   echo longpathsOK=%longpathsOK%
   if %longpathsOK%==1 (echo Long paths are now OK.) else (echo longpathsOK=%longpathsOK%)
+  )
 )
 
 If exist %gitfilename% (ECHO %gitfilename% is there!) ELSE (
-  ECHO Downloading Git %gitversion% %gitsize%...
+  ECHO Downloading Git %gitversion% %gitsize:"=%...
   ECHO Check that your internet is on and
   pause
   powershell.exe -noprofile -command "Invoke-WebRequest %giturl% -OutFile %gitfilename%"
@@ -110,8 +112,8 @@ If exist %gitfilename% (ECHO %gitfilename% is there!) ELSE (
 ECHO Installing Git %gitversion%
 ::/SP- not needed?
 ::Pick one of these; VERYSILENT is not visible to the user, SILENT shows progress
-start %gitfilename% /SILENT /NORESTART /NOCANCEL /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS="icons,ext\shellhere,assoc,assoc_sh"
-::start %gitfilename% /VERYSILENT /NORESTART /NOCANCEL /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS="icons,ext\shellhere,assoc,assoc_sh"
+start /wait %gitfilename% /SILENT /NORESTART /NOCANCEL /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS="icons,ext\shellhere,assoc,assoc_sh"
+::start /wait %gitfilename% /VERYSILENT /NORESTART /NOCANCEL /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS="icons,ext\shellhere,assoc,assoc_sh"
 
 ::The above line should be commented out to try this block:
 ::(echo [Setup]
@@ -142,41 +144,48 @@ start %gitfilename% /SILENT /NORESTART /NOCANCEL /CLOSEAPPLICATIONS /RESTARTAPPL
 ::The problem at this point is that we can't find the git executable,
 ::I think because the path cannot be updated at this point in the script. So we need to call git from a second script, at this point
 
-::This is the second script
+echo writing the second script ^(%~dpn0-2%~x0^)
+::What to do with this logic? do causes problems in the echo script...
+::echo set azt=
+::echo for /R %%f in (azt.git) do @IF EXIST %%f set azt=%%~dpnxf
+::echo if defined azt (
+::echo echo azt is defined ^(local repo found^): %azt%
+::echo ) else (
+
+::This may also be useless:
+::ECHO ::WshShell = CreateObject("Wscript.shell")
+::ECHO ::strDesktop = WshShell.SpecialFolders("Desktop")
+::ECHO ::oMyShortcut = WshShell.CreateShortcut(strDesktop + "\A-Z+T.lnk")
+::ECHO ::oMyShortcut.WindowStyle = 3 &&Maximized 7=Minimized 4=Normal
+::ECHO ::oMyShortcut.IconLocation = "%userprofile%/desktop/azt/AZT stacks6_icon.png"
+::ECHO ::OMyShortcut.TargetPath = "%userprofile%/desktop/azt/main.py"
+::ECHO ::oMyShortCut.Hotkey = "ALT+CTRL+F"
+::ECHO ::oMyShortCut.Save
 (
 echo @echo off
-echo ECHO Cloning A-Z+T source to '%userprofile%/desktop/azt'
+echo ECHO Cloning A-Z+T source to '%userprofile%\desktop\azt'
 echo ::This is for local ^(USB^) repo install
-echo set azt=
-echo for /R %%f in (azt.git) do @IF EXIST %%f set azt=%%~dpnxf
-echo if defined azt (
-echo echo azt is defined ^(local repo found^): %azt%
-echo ) else (
 echo echo Local file not found; using github
 echo set azt=https://github.com/kent-rasmussen/azt.git
 echo git clone %azt% ^"%userprofile%/desktop/azt^"
-echo echo confirm this is what you want, then
+echo echo confirm this is what you want^, then
 echo ECHO making links to AZT and Transcriber tool...
-echo mklink "%userprofile%/desktop/A-Z+T" "%userprofile%/desktop/azt/main.py"
-echo mklink "%userprofile%/desktop/Transcriber" "%userprofile%/desktop/azt/transcriber.py"
-ECHO ::WshShell = CreateObject("Wscript.shell")
-ECHO ::strDesktop = WshShell.SpecialFolders("Desktop")
-ECHO ::oMyShortcut = WshShell.CreateShortcut(strDesktop + "\A-Z+T.lnk")
-ECHO ::oMyShortcut.WindowStyle = 3 &&Maximized 7=Minimized 4=Normal
-ECHO ::oMyShortcut.IconLocation = "%userprofile%/desktop/azt/AZT stacks6_icon.png"
-ECHO ::OMyShortcut.TargetPath = "%userprofile%/desktop/azt/main.py"
-ECHO ::oMyShortCut.Hotkey = "ALT+CTRL+F"
-ECHO ::oMyShortCut.Save
+echo echo these lines will fail if you didn't run this script as administrator
+echo echo if they fail^, close this window ^(X^) and run again^, as administrator.
 echo pause
 echo del %~dpn0-2%~x0
 ) >%~dpn0-2%~x0
 
+:::will these work before the location exists?
+echo mklink "%userprofile%/desktop/A-Z+T" "%userprofile%/desktop/azt/main.py"
+echo mklink "%userprofile%/desktop/Transcriber" "%userprofile%/desktop/azt/transcriber.py"
 
-
+echo Right-click on %~dpn0-2%~x0 and run it As Administrator
+pause
 ECHO Install done! ^(hopefully!^)
 
 ECHO I'll pause now; cancel now to be finished, or press any key to continue
-ECHO to install XLingPaper, Praat and Mercurial, to get the most out of A-Z+T.
+ECHO to install XLingPaper^, Praat and Mercurial^, to get the most out of A-Z+T.
 pause
 
 ::cd /d "%userprofile%/Downloads"
