dc8c74eadacc685ce42053613796ac77fb11ae7a
Kent Rasmussen <kent_rasmussen@sil.org>
2024-07-06 19:06:10 -0500

    for testing

--- main.py
+++ main.py
@@ -14836,8 +14836,14 @@ def praatversioncheck():
         return True
     try:
         characters=stouttostr(versionraw)
-        if characters.count(' ') >4: #normal: praat version (month day year)
+        log.info("versionraw={}".format(versionraw))
+        log.info("characters1={}".format(characters))
+        log.info("charactersr={}".format(characters.count(r'\x00')))
+        log.info("charactersr={}".format(r'\x00' in versionraw))
+        log.info("charactersb={}".format(b'\x00' in versionraw))
+        if characters.count(r'\x00') >4: #normal: praat version (month day year)
             characters=versionraw.decode('utf-16')
+            log.info("characters1={}".format(characters))
         out=version.Version(parseversion(characters))
     except Exception as e:
         log.info("Problem getting parseable praat version ({})".format(e))
