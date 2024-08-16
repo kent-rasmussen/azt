d88218457b51c2dfb1a9f8ed32183cd3445b5d5e
Kent Rasmussen <kent_rasmussen@sil.org>
2024-08-16 11:54:18 -0500

    this will never be present and false

--- sound_ui.py
+++ sound_ui.py
@@ -61,7 +61,7 @@ class RecordButtonFrame(ui.Frame):
         # self.p.bind('<ButtonRelease>', self.function)
         self.p.grid(row=0, column=1,sticky='w')
         pttext=_("Click to hear")
-        if program['praat']:
+        if 'praat' in program:
             pttext+='; '+_("right click to open in praat")
             self.p.bind('<Button-3>',lambda x: praatopen(self._filenameURL))
         self.pt=ui.ToolTip(self.p,pttext)
