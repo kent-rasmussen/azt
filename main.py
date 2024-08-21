6f9c64ed6293caae2392268f8cdbd91ac1bc601a
Kent Rasmussen <kent_rasmussen@sil.org>
2024-08-21 17:06:39 -0500

    note

--- main.py
+++ main.py
@@ -6412,6 +6412,7 @@ class Parse(Segments):
             self.parser.autolevel(5) #no auto
             self.parser.asklevel(0)
         self.ftype=program['params'].ftype('lc') #Is this always correct?
+        # self.ftype=program['params'].ftype('lx') #I think once we parse, we want this
         # self.nodetag='citation'
         self.dodone=True #give me words with citation done
         self.checkeach=False #don't confirm each word (default)
