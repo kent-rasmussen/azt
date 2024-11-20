6a814a97d4e43b09157c47b1e8a8d4c1270a8eb9
Kent Rasmussen <kent_rasmussen@sil.org>
2024-11-20 14:07:16 -0600

    use this fn more generally

--- utilities.py
+++ utilities.py
@@ -16,3 +16,39 @@ def tryrun(cmd):
         text=_("{} command error: {}\n({})").format(cmd.__name__,e,cmd)
         log.error(text)
         ErrorNotice(text,title=_("{} command error!").format(cmd.__name__))
+def indenteddict(indict):
+    outdict={}
+    # log.info("working on dict with keys {}".format(indict.keys()))
+    for j in indict:
+        # log.info("working on {}".format(j))
+        if isinstance(indict[j], dict):
+            # log.info("printing indented dict for {} key".format(j))
+            # config[s][j]='\n'.join(['{'+i+':'+str(v[j][i])+'}'
+            #                             for i in v[j].keys()])
+            if True in [isinstance(i, dict) for i in indict[j].values()]:
+                # log.info("printing double indented dict for {}: {} "
+                #             "keys".format(j,indict[j].keys()))
+                outdict[j]='{'+',\n'.join(
+                    [quote(k)+':{'+',\n\t'.join(
+                                        [quote(i)+':'+quote(indict[j][k][i])
+                                            for i in indict[j][k]#.keys()
+                                            # for k in indict[j].keys()
+                                            if i #and i in indict[j][k].keys()
+                                        ]
+                                                )+'}'
+                    for k in indict[j]#.keys()
+                    if k #and k in indict[j].keys()
+                    # if k
+                    ]
+                                            )+'}'
+                # '\n\t\t'.join(str({i:v[j][k][i]
+                #                             for i in v[j][k]}))
+            else:
+                log.info(_("printing indented dict for {} key").format(j))
+                outdict[j]='{'+',\n'.join([quote(i)+':'+quote(indict[j][i])
+                                        for i in indict[j]#.keys()
+                                        if i])+'}'
+        elif indict[j]:
+            # log.info(_("printing unindented dict for {} key").format(j))
+            outdict[j]=str(indict[j]) #don't quote booleans!
+    return outdict
