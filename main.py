9407267b40ade3b95bbc908753dbc07877c2a728
Kent Rasmussen <kent_rasmussen@sil.org>
2025-01-15 13:16:12 -0600

    make image initialization more portable

--- main.py
+++ main.py
@@ -397,9 +397,10 @@ class LiftChooser(ui.Window,HasMenus):
             # log.info("Working with image files {}".format(
             #                                 file.getfilesofdirectory(sense.imgselectiondir)))
             if sense.cawln and not sense.illustrationvalue():
+                dir=file.fullpathname(sense.imgselectiondir)
                 # log.info("Found CAWL sense without image field")
-                if file.exists(sense.imgselectiondir):
-                    urls=file.getfilesofdirectory(sense.imgselectiondir)
+                if file.exists(dir):
+                    urls=file.getfilesofdirectory(dir)
                     if urls:
                         filename=sense.imagename() #new file name
                         # log.info("Working with image filename {}".format(filename))
