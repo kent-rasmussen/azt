0d3c269df02e3d85c4afa83c7e2ed625bbe1193c
Kent Rasmussen <kent_rasmussen@sil.org>
2025-01-31 18:20:37 -0600

    log

--- main.py
+++ main.py
@@ -5326,6 +5326,7 @@ class TaskChooser(TaskDressing,ui.Window):
         sortsrecorded=program['db'].nfieldswsoundfiles
         log.info("nfieldswsoundfiles by lang: {}".format(sortsrecorded))
         sortsnotrecorded={}
+        log.info(f"sorts: {sorts}")
         if me:
             enough=0
         else:
