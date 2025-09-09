a5a19b8b02401e4e0a54b37c6fa561e1daf15aa9
Kent Rasmussen <kent_rasmussen@sil.org>
2025-09-09 17:08:27 -0500

    for testing

--- asr.py
+++ asr.py
@@ -31,3 +31,45 @@ class ASRtoText(object):
         # model = whisper.load_model("small")
         #model = whisper.load_model("medium")
         #model = whisper.load_model("large")
+if __name__ == '__main__':
+    print(datetime.datetime.now())
+    asr=ASRtoText(
+            simplify_length=True,
+            simplify_nasals=True,
+            simplify_vowels=True,
+            simplify_dipthongs=True,
+            simplify_offglides=True,
+            mms=True,
+            neurlang=True,
+            # allosaurus=True,
+            whisper_base=True, #This should only be for LWC transcription
+            show_tone=True,
+            # other_models=True,
+            cache_dir='/media/kentr/hfcache',
+            sister_languages=['swa'],
+            sister_language=None
+        )
+    print(datetime.datetime.now())
+    test={1:'/home/kentr/bin/raspy/azt/John_01_zmb_NP.wav',
+        2:'/home/kentr/bin/raspy/azt/John_01_zmb_word.wav'}
+    for i in test.values():
+        log.info(f"{asr.return_ipa=}")
+        asr.get_transcriptions(i)
+        log.info(f"{asr.return_ipa=}")
+        # for m in asr.inferall(i):
+        #     print(m)
+        # print("Show tone:",asr.show_tone)
+    quit()
+    asr.sister_languages=['sna']
+    for asr.repo in asr.models:
+        if 'facebook' in asr.repo:
+            asr.load_ctc_adaptor()
+            asr.model=asr.models[asr.repo]
+            for i in test.values():
+                print(asr.infer(i))
+    print(datetime.datetime.now())
+    # for i in test.values():
+    #     for m in asr.inferall(i):
+    #         print(m)
+    #     print("Tone:",asr.tone)
+    # print(datetime.datetime.now())
