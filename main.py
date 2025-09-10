73ccb48639e285783909081890f7c7180e84f1ee
Kent Rasmussen <kent_rasmussen@sil.org>
2025-09-10 12:45:43 -0500

    make langtags available

--- main.py
+++ main.py
@@ -68,6 +68,8 @@ except ModuleNotFoundError as e:
 from utilities import *
 import executables
 import export
+import langtags
+program['languages']=langtags.Languages()
 try:
     import sound
     import transcriber
