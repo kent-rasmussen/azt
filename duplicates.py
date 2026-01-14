97be01b14795d41eedd75adb5206286f87c34633
Kent Rasmussen <kent_rasmussen@sil.org>
2026-01-14 16:34:21 -0600

    works better

--- duplicates.py
+++ duplicates.py
@@ -5,18 +5,22 @@ import psutil
 import pathlib
 
 def running_file(path):
+    resolved=pathlib.Path(path).resolve()
     psutil.process_iter.cache_clear()
+
     l=list()
     for q in psutil.process_iter([]):
         try:
-            for c in q.cmdline():
-                if pathlib.Path(path).resolve() == pathlib.Path(c).resolve():
-                    l.append(q.cmdline())
-                # elif 'main.py' in c:
-                #     l.append(q.cmdline())
+            qcmd=q.cmdline()
+            if '-X' not in qcmd:
+                continue
+            for c in qcmd:
+                if resolved == pathlib.Path(c).resolve():
+                    l.append(qcmd)
         except psutil.ZombieProcess:
             continue
     if len(l)>1:
-        print(f"\n{pathlib.Path(path).resolve()} is already running:\n\n",l)
+        print(f"\n{pathlib.Path(path).resolve()} is already running:\n\n",
+                '\n'.join([str(i) for i in l]))
         input('\nPress ENTER to exit\n')
         return True
