329c6656d694b2e0946272b31b7af114e74d5f6f
Kent Rasmussen <kent_rasmussen@sil.org>
2026-01-15 07:50:38 -0600

    try and don't fail for Windows error

--- duplicates.py
+++ duplicates.py
@@ -12,21 +12,25 @@ def running_file(path):
         ok_processes+=1
     resolved=pathlib.Path(path).resolve()
     # psutil.process_iter.cache_clear() #doesn't seem to help
-    if use_list_comprehension:
-        l=[q.info['cmdline'] for q in psutil.process_iter(['cmdline'])
-            if q.info['cmdline'] and '-X' not in q.info['cmdline']
-                    and not [c for c in q.info['cmdline'] if 'py.exe' in c]
-            and resolved in [pathlib.Path(c).resolve() for c in q.info['cmdline']]
-            ]
-    else:
-        l=list() #may be less efficient
-        for q in psutil.process_iter(['cmdline']):
-            qcmd=q.info['cmdline']
-            if qcmd is None or '-X' in qcmd: #avoids need for try/except
-                continue
-            for c in qcmd:
-                if resolved == pathlib.Path(c).resolve():
-                    l.append(qcmd)
+    try:
+        if use_list_comprehension:
+            l=[q.info['cmdline'] for q in psutil.process_iter(['cmdline'])
+                if q.info['cmdline'] and '-X' not in q.info['cmdline']
+                        and not [c for c in q.info['cmdline'] if 'py.exe' in c]
+                and resolved in [pathlib.Path(c).resolve() for c in q.info['cmdline']]
+                ]
+        else:
+            l=list() #may be less efficient
+            for q in psutil.process_iter(['cmdline']):
+                qcmd=q.info['cmdline']
+                if qcmd is None or '-X' in qcmd: #avoids need for try/except
+                    continue
+                for c in qcmd:
+                    if resolved == pathlib.Path(c).resolve():
+                        l.append(qcmd)
+    except OSError as e:
+        print(f"OS Error checking for running file: {e}")
+        return
     if len(l)>ok_processes:
         import locale
         loc,enc=locale.getlocale()
