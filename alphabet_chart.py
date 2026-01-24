d8622495c247a0e84e6be36c82de9a19d4eb8ffa
Kent Rasmussen <kent_rasmussen@sil.org>
2026-01-24 06:15:03 -0600

    regularize PDF text, limit to one occurrance of each letter in alphabetic order

--- alphabet_chart.py
+++ alphabet_chart.py
@@ -275,7 +275,7 @@ class OrderAlphabet(ui.Window):
                 (_("Pictured Only"), self._show_pictured_only),
                 (_("Show All"), self._show_all),
                 (_("Charis"), self.toggle_font),
-                (_("Print"), self.print_chart)]:
+                (_("PDF"), self.print_chart)]:
             self.config_buttons[text]=ui.Button(self.configFrame,
                                         text=text, command=command,
                                         r=self.configFrame.grid_size()[1], c=0)
@@ -467,6 +467,7 @@ class OrderAlphabet(ui.Window):
         #pick up new letters, limit to actual but keep order
             self.order=sorted(gd-set([str(i) for i in self.order])
                             )+[str(i) for i in self.order if i in gd] 
+        self.order=[i for n,i in enumerate(self.order) if n==self.order.index(i)]
         log.info(f"Using this alphabetical order: {self.order}")
         log.info(f"Using these exids: {self.exids}")
         if self.exids:
