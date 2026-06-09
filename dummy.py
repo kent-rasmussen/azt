dbe730adb2264e480eb484253eaad144fb31f40a
Kent Rasmussen <kent_rasmussen@sil.org>
2026-06-09 09:58:29 -0500

    generalize and distinguish

--- dummy.py
+++ dummy.py
@@ -2,13 +2,16 @@
 # coding=UTF-8
 
 class App(object):
-    def __init__(self):
+    def __init__(self,program_dict=None):
         super().__init__()
+        self.dummy=True #lets ui.Root distinguish this from the real App
         self.name='tkinter UI module'
         self.url='https://github.com/kent-rasmussen/azt'
         self.Email='kent_rasmussen@sil.org'
         self.theme='Kim'
-
+        for k in program_dict:
+            setattr(self,k,program_dict[k])
+        
 class TaskChooser(object):
     def __init__(self):
         super().__init__()
